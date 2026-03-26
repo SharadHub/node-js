6031f32358b58390af7c43578915a8bedc58a63b
Sharad Bista <bishalbista737@gmail.com>
2026-03-26 22:55:21 +0545

    chore:node.js learning repo

---
+++ chapter-two/async.js
@@ -0,0 +1,29 @@
+
+function checkingInventory(){
+    console.log("checking inventory........");
+}
+
+function createOrder(){
+    console.log("creating order..........");
+
+}
+
+function chargepayment(){
+    console.log("charge payment...........");
+
+}
+
+function sendInvoice(){
+    console.log("sending invoice........");
+
+}
+
+function main(){
+    checkingInventory();
+    createOrder();
+    chargepayment();
+    sendInvoice();
+}
+
+
+main();
